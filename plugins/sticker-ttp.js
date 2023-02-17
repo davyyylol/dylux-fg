f881f22d2a5b26f36a7cf3f888ed5034f74d2fa8
davyyylol <125281187+davyyylol@users.noreply.github.com>
2023-02-17 23:27:06 +0100

    Update sticker-ttp.js

--- plugins/sticker-ttp.js
+++ plugins/sticker-ttp.js
@@ -3,7 +3,7 @@ import { sticker } from '../lib/sticker.js'
 import fg from 'api-dylux'
 let handler = async (m, { conn, text, usedPrefix, command }) => {
     
-    if (!text) throw `✳️ Envie el texto\n\n📌Ejemplo *${usedPrefix + command}* dylux-fg`  
+    if (!text) throw `✳️ Invia il testo\n\n📌Esempio *${usedPrefix + command}* Zeno`  
     let color = '2FFF2E' //color
     let res = await fg.ttp(text, color) 
     let stiker = await sticker(null, res.result, global.packname, global.author)
