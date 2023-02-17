40c4de98ea107b7069784560a4703ae7a9c4e2e5
davyyylol <125281187+davyyylol@users.noreply.github.com>
2023-02-17 23:19:29 +0100

    Update sticker-attp.js

--- plugins/sticker-attp.js
+++ plugins/sticker-attp.js
@@ -1,7 +1,7 @@
 
 import { sticker } from '../lib/sticker.js'
 let handler = async (m, { conn, text, usedPrefix, command }) => {
-	if (!text) throw `✳️ _Envie el texto_\n\n📌Ejemplo *${usedPrefix + command}* dylux-fg`
+	if (!text) throw `✳️ _Invia il testo_\n\n📌Esempio *${usedPrefix + command}* Zeno`
     //conn.sendFile(m.chat, global.API('xteam', '/attp', { file: '', text }), 'attp.webp', '', m, false, { asSticker: true })
      let stiker = await sticker(null, global.API('xteam', '/attp', { file: '', text }), global.packname, global.author)
     if (stiker) return conn.sendFile(m.chat, stiker, 'sticker.webp', '', m )
