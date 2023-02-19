e679612ff9d10f3614dd45c16a83f3b4b2e0a1b7
davyyylol <125281187+davyyylol@users.noreply.github.com>
2023-02-19 17:34:29 +0100

    Update handler.js

--- handler.js
+++ handler.js
@@ -482,7 +482,7 @@ export async function participantsUpdate({ id, participants, action }) {
                         ppgp = await this.profilePictureUrl(id, 'image')
                         } finally {
                         text = (action === 'add' ? (chat.sWelcome || this.welcome || conn.welcome || 'Bienvenido, @user').replace('@group', await this.getName(id)).replace('@desc', groupMetadata.desc?.toString() || 'Desconocido') :
-                            (chat.sBye || this.bye || conn.bye || 'Adiós, @user')).replace('@user', '@' + user.split('@')[0])
+                            (chat.sBye || this.bye || conn.bye || 'Addio @user')).replace('@user', '@' + user.split('@')[0])
                          
                             let wel = API('fgmods', '/api/welcome', {
                                 username: await this.getName(user),
