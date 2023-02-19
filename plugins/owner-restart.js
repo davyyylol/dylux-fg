963091af3d413d90229a6e5a9ea2a448ca9a2bad
davyyylol <125281187+davyyylol@users.noreply.github.com>
2023-02-19 20:45:28 +0100

    Update owner-restart.js

--- plugins/owner-restart.js
+++ plugins/owner-restart.js
@@ -2,7 +2,7 @@ import { spawn } from 'child_process'
 let handler = async (m, { conn, isROwner, text }) => {
     if (!process.send) throw 'Dont: node main.js\nDo: node index.js'
     if (conn.user.jid == conn.user.jid) {
-    await m.reply('🔄 Reiniciando Bot...\n Espere un momento')
+    await m.reply('🔄 Riavviando il bot...\n Aspetta circa 20 secondi')
     process.send('reset')
   } else throw 'eh'
 }
