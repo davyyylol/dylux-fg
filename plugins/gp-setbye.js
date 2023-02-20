977cb59f446d60cae920b80874c787da9b339b45
davyyylol <125281187+davyyylol@users.noreply.github.com>
2023-02-20 20:02:42 +0100

    Update gp-setbye.js

--- plugins/gp-setbye.js
+++ plugins/gp-setbye.js
@@ -5,6 +5,7 @@ let handler = async (m, { conn, text, isROwner, isOwner }) => {
     global.db.data.chats[m.chat].sBye = text
     m.reply('✅ Messaggio di abbandono impostato')
   } else throw `✳️ Inserisci il messaggio\nEsempio: /setbye Addio @user
+  
   Prefissi disponibili:
   @user (tag della persona che ha abbandonato)`
 }
