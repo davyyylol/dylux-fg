a7889228b6f4f465e920e39706d0856471191aba
davyyylol <125281187+davyyylol@users.noreply.github.com>
2023-04-04 14:12:34 +0200

    Update gp-delwarn.js

--- plugins/gp-delwarn.js
+++ plugins/gp-delwarn.js
@@ -7,11 +7,12 @@ let handler = async (m, { conn, args, groupMetadata}) => {
         if (!(who in global.db.data.users)) throw `✳️ Utente non trovato`
        let warn = global.db.data.users[who].warn
        if (warn > 0) {
-         global.db.data.users[who].warn -= 2
-         m.reply(`⚠️ *Reset warn*
+         global.db.data.users[who].warn -= 1
+         m.reply(`⚠️ *Rimuovi warn*
          
-✅ I warn dell'utente sono stati resettati`)
-         m.reply(`✳️ Un amministratore del gruppo ha resettato i tuoi warn, ora hai *${warn - 2}*`, who)
+▢ Warn rimossi: *-1*
+▢ Warn correnti: *${warn - 1}*`)
+         m.reply(`✳️ Un amministratore del gruppo ha rimosso un warn, ora hai *${warn - 1}*`, who)
          } else if (warn == 0) {
             m.reply('✳️ Questo utente non ha warn')
         }
